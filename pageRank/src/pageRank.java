b56e6c419547ba03a78c393d4a78331ebe91c50f
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-09 14:02:50 -0700

    add second map

--- pageRank/src/pageRank.java
+++ pageRank/src/pageRank.java
@@ -8,6 +8,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -15,14 +17,14 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class pageRank {
 
-    public static class PageRankMapper extends
+    public static class TransitionMapper extends
             Mapper<Object, Text, Text, Text> {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             
             String line = value.toString();            
             line = line.trim();            
-            String[] transtion = line.split("\t"); //split by ' ', '\t'...ect
+            String[] transtion = line.split("\t");
             String from = transtion[0];
             String[] to = transtion[1].split(",");
             double length = (double)1/to.length;
@@ -31,6 +33,20 @@ public class pageRank {
             }
         }
     }
+
+    public static class PRMapper extends
+            Mapper<Object, Text, Text, Text> {
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            
+            String line = value.toString();            
+            line = line.trim();            
+            String[] transtion = line.split("\t");
+            String from = transtion[0];
+            double weight = Double.parseDouble(transtion[1]);
+            context.write(new Text(from), new Text(""+weight));
+        }
+    }   
 
     public static class PageRankReducer extends
             Reducer<Text, Text, Text, Text> {
@@ -48,20 +64,27 @@ public class pageRank {
         Job job = new Job(conf, "page rank");
         job.setJarByClass(pageRank.class);
 
-        job.setMapperClass(PageRankMapper.class);
+        ChainMapper.addMapper(job, TransitionMapper.class, Object.class, Text.class, Text.class, Text.class, conf);
+        ChainMapper.addMapper(job, PRMapper.class, Object.class, Text.class, Text.class, Text.class, conf);
+
+        //job.setMapperClass(PageRankMapper.class);
         job.setReducerClass(PageRankReducer.class);
 
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
+        //job.setMapOutputKeyClass(Text.class);
+        //job.setMapOutputValueClass(Text.class);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        job.setInputFormatClass(TextInputFormat.class);
+        //job.setInputFormatClass(TextInputFormat.class);
+        //job.setOutputFormatClass(TextOutputFormat.class);
+
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TransitionMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, PRMapper.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        //FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         job.waitForCompletion(true);
     }
